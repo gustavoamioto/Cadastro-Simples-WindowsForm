bc0ca7fa2f3059d0124f36ac9aa3fa0f0001c3f2
gustavoamioto <contato.gustavomioto@gmail.com>
2022-02-18 15:59:30 -0300

    Implementação GridView com informações de Clientes do Banco de Dados

--- projetoum/Properties/Resources.Designer.cs
+++ projetoum/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace projetoum.Properties {
+namespace projetocadastros.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace projetoum.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("projetoum.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("projetocadastros.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
